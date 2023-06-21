b2c56e99e1ce36ad58deaf62190f9056636749de
bun133 <bun133.work@gmail.com>
2023-06-21 18:22:01 +0900

    fix styling /tickets/[id]

--- e-syoku-front/app/tickets/[id]/page.tsx
+++ e-syoku-front/app/tickets/[id]/page.tsx
@@ -30,9 +30,9 @@ export default function Page() {
             <div>
                 <PageTitle title={"食券番号 " + ticket.ticketNum}></PageTitle>
                 <div
-                    className={"w-full h-max p-3 mx-2 shadow-2xl rounded-2xl flex flex-col justify-center items-start"}>
+                    className={"h-max p-3 mx-2 shadow-2xl rounded-2xl flex flex-col justify-center items-start"}>
                     <div
-                        className={"w-full py-5 m-2 flex flex-col items-center justify-center bg-gray-100 rounded-2xl space-y-1"}>
+                        className={"w-full py-5 my-2 flex flex-col items-center justify-center bg-gray-100 rounded-2xl space-y-1"}>
                         <div className={""}>食券番号</div>
                         <div className={"font-bold text-xl"}>{ticket.ticketNum}</div>
                     </div>
