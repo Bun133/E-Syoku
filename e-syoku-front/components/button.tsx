d5d697c689d86a3dabb796f608e795abf6adc55a
bun133 <bun133.work@gmail.com>
2023-06-21 21:01:13 +0900

    update button without href

--- e-syoku-front/components/button.tsx
+++ e-syoku-front/components/button.tsx
@@ -2,11 +2,20 @@ import React from "react";
 import Link from "next/link";
 
 export default function Button(props: {
-    href: string,children: React.ReactNode
+    href?: string, children: React.ReactNode,
+    onClick?: () => void
 }) {
-    return (
-        <Link href={props.href} className="p-2 bg-green-400 rounded-xl m-2">
-            {props.children}
-        </Link>
-    );
+    if (props.href) {
+        return (
+            <Link href={props.href} className="p-2 bg-green-400 rounded-xl m-2" onClick={props.onClick}>
+                {props.children}
+            </Link>
+        );
+    } else {
+        return (
+            <div className="p-2 bg-green-400 rounded-xl m-2 hover:cursor-pointer hover:bg-green-500" onClick={props.onClick}>
+                {props.children}
+            </div>
+        )
+    }
 }
