dbc7c5e81a4c5c7decbc6e6a7f3a52ed24f8f95a
bun133 <bun133.work@gmail.com>
2023-07-02 20:41:13 +0900

    add OrderSelection Complete

--- e-syoku-front/components/form/OrderSelection.tsx
+++ e-syoku-front/components/form/OrderSelection.tsx
@@ -1,12 +1,12 @@
 import {GoodsRemainData, GoodsWithRemainData, Order} from "@/lib/e-syoku-api/Types";
 import {SimpleGrid, Spacer} from "@chakra-ui/react";
 import Goods from "@/components/goods";
-import {useRef, useState} from "react";
-import {Heading, HStack} from "@chakra-ui/layout";
+import {useState} from "react";
+import {Center, Heading, HStack, VStack} from "@chakra-ui/layout";
 import Btn from "@/components/btn";
 
 export function OrderSelection(param: { goods: GoodsWithRemainData[], callBack: (order: Order) => void }) {
-    const listRefs = useRef<number[]>([])
+    const [listRefs, setListRefs] = useState<number[]>([])
 
     function isRemain(r: GoodsRemainData) {
         // @ts-ignore
@@ -22,20 +22,42 @@ export function OrderSelection(param: { goods: GoodsWithRemainData[], callBack: 
         throw new Error("remain is not defined")
     }
 
+    function isPossibleToEnd() {
+        return listRefs.some(r => r > 0)
+    }
+
+    function generateOrder(): Order {
+        return param.goods.map((g, index) => {
+            return {
+                goodsId: g.key.goodsId,
+                count: listRefs[index]
+            }
+        }).filter(o => o.count > 0)
+    }
+
 
     return (
-        <SimpleGrid spacing={4} templateColumns={"repeat(2, 1fr)"} w={"100%"} h={"100%"} p={4}>
-            {param.goods.map((g, index) => {
-                return (
-                    <Goods goods={g.key}
-                           footer={(
-                               <OrderSelectionFooter onChange={(to) => {
-                                   listRefs.current[index] = to
-                               }}
-                                                     isDisabled={!isRemain(g.value)}/>)}></Goods>
-                )
-            })}
-        </SimpleGrid>
+        <VStack>
+            <SimpleGrid spacing={4} templateColumns={"repeat(2, 1fr)"} w={"100%"} h={"100%"} p={4}>
+                {param.goods.map((g, index) => {
+                    return (
+                        <Goods goods={g.key}
+                               footer={(
+                                   <OrderSelectionFooter onChange={(to) => {
+                                       const copy = listRefs.slice()
+                                       copy[index] = to
+                                       setListRefs(copy)
+                                   }}
+                                                         isDisabled={!isRemain(g.value)}/>)}></Goods>
+                    )
+                })}
+            </SimpleGrid>
+            <Center>
+                <Btn disabled={!isPossibleToEnd()} onClick={() => {
+                    param.callBack(generateOrder())
+                }}>決定</Btn>
+            </Center>
+        </VStack>
     )
 }
 
@@ -45,16 +67,19 @@ export function OrderSelectionFooter(param: { onChange: (to: number) => void, ma
     return (
         <HStack>
             <Btn onClick={() => {
-                setCount(Math.max(0, count - 1))
-                param.onChange(count)
+                const toUpdate = Math.max(0, count - 1)
+                setCount(toUpdate)
+                param.onChange(toUpdate)
             }} disabled={param.isDisabled}>-</Btn>
             <Spacer/>
             <Heading>{count}</Heading>
             <Spacer/>
             <Btn onClick={() => {
-                if (param.max) setCount(Math.min(param.max, count + 1))
-                else setCount(Math.max(0, count + 1))
-                param.onChange(count)
+                let toUpdate
+                if (param.max) toUpdate = Math.min(param.max, count + 1)
+                else toUpdate = Math.max(0, count + 1)
+                setCount(toUpdate)
+                param.onChange(toUpdate)
             }} disabled={param.isDisabled}>+</Btn>
         </HStack>
     )
