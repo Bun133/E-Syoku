06b0709f81dbf5274a89981dd4d2a7ec2d534c18
bun133 <bun133.work@gmail.com>
2023-07-02 14:02:38 +0900

    add login page

---
+++ e-syoku-front/app/auth/login/page.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import PageTitle from "@/components/pageTitle";
+import Btn from "@/components/btn";
+import {useFirebaseAuth} from "@/lib/firebase/authentication";
+import {Auth, GoogleAuthProvider, signInWithPopup} from "@firebase/auth";
+import {Loader} from "react-feather";
+import {Center} from "@chakra-ui/layout";
+
+export default function () {
+    const context = useFirebaseAuth()
+    const auth = context.auth
+    if (!auth) {
+        return (
+            <div>
+                <PageTitle title={"ログイン"}></PageTitle>
+                <Center>
+                    <Loader></Loader>
+                </Center>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <PageTitle title={"ログイン"}></PageTitle>
+            <Center>
+                <Btn onClick={async () => {
+                    const r = await loginWithGoogle(auth)
+                    console.log("Login result", r)
+                }}>Googleでログイン</Btn>
+            </Center>
+        </div>
+    )
+}
+
+async function loginWithGoogle(auth: Auth) {
+    return await signInWithPopup(auth, new GoogleAuthProvider())
+}
