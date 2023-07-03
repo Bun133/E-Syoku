9d02d094533635fdd6a3747daf8df047f91734b3
bun133 <bun133.work@gmail.com>
2023-07-03 17:45:40 +0900

    add getGoodsById,remainDataIsEnough

--- e-syoku-backend/functions/src/impls/goods.ts
+++ e-syoku-backend/functions/src/impls/goods.ts
@@ -1,8 +1,14 @@
-import {Goods, goodsRemainDataSchema, goodsSchema} from "../types/goods";
+import {Goods, GoodsRemainData, goodsRemainDataSchema, goodsSchema} from "../types/goods";
 import {DBRefs, parseData} from "../utils/db";
 import {firestore} from "firebase-admin";
 import {UniqueId} from "../types/types";
+import {error} from "../utils/logger";
 import DocumentReference = firestore.DocumentReference;
+
+export async function getGoodsById(ref: DBRefs, goodsId: UniqueId): Promise<Goods | undefined> {
+    const directRef = ref.goods.doc(goodsId)
+    return await getGoodsFromRef(directRef)
+}
 
 export async function getGoodsFromRef(ref: DocumentReference): Promise<Goods | undefined> {
     return await parseData(goodsSchema, ref, (data) => {
@@ -31,4 +37,23 @@ export async function getRemainDataOfGoods(refs: DBRefs, goodsId: UniqueId) {
             ...data
         }
     })
+}
+
+/**
+ * GoodsRemainDataに対して、必要個数が揃っているかどうかの確認をします
+ * @param remainData
+ * @param quantity
+ */
+export function remainDataIsEnough(remainData: GoodsRemainData, quantity: number): undefined | boolean {
+    // @ts-ignore
+    if (remainData.remain != undefined && typeof remainData.remain == "boolean") {
+        // @ts-ignore
+        return remainData.remain as boolean
+        // @ts-ignore
+    } else if (remainData.remainCount != undefined && typeof remainData.remainCount == "number") {
+        // @ts-ignore
+        return remainData.remainCount >= quantity
+    }
+    error("in remainDataIsEnough cannot decide what type the data is.Data:", remainData)
+    return undefined
 }
