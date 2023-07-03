593bd46f92255fd2a232a7b956ac69631a14ab77
bun133 <bun133.work@gmail.com>
2023-07-03 16:47:43 +0900

    add Order Screen

--- e-syoku-front/app/order/page.tsx
+++ e-syoku-front/app/order/page.tsx
@@ -3,17 +3,54 @@ import {useEndpoint} from "@/lib/e-syoku-api/Axios";
 import {listGoodsEndPoint} from "@/lib/e-syoku-api/EndPoints";
 import PageTitle from "@/components/pageTitle";
 import {Loader} from "react-feather";
-import {Center} from "@chakra-ui/layout";
-import Goods from "@/components/goods";
+import {Center, VStack} from "@chakra-ui/layout";
 import {OrderSelection} from "@/components/form/OrderSelection";
+import {useState} from "react";
+import {Order} from "@/lib/e-syoku-api/Types";
+import Goods from "@/components/goods";
+import Btn from "@/components/btn";
 
 export default function () {
-    const {response: data} = useEndpoint(listGoodsEndPoint, {})
-    if (data) {
+    const {response: itemsData} = useEndpoint(listGoodsEndPoint, {})
+    const [order, setOrder] = useState<Order>()
+
+    if (order && itemsData) {
+        return (
+            <div>
+                <PageTitle title={"注文確定画面"}></PageTitle>
+                <VStack>
+                    {order.map((orderData, index) => {
+                        const item = itemsData.data?.data.find((item) => item.key.goodsId === orderData.goodsId)
+                        if (!item) return null
+                        return (
+                            <Goods key={index} goods={item.key} footer={(<div>
+                                注文個数:{orderData.count}
+                            </div>)}></Goods>
+                        )
+                    })}
+
+                    <Center>
+                        <Btn onClick={() => {
+                            // TODO send order
+                        }}>注文を確定</Btn>
+                    </Center>
+                </VStack>
+            </div>
+        )
+    }
+
+    if (itemsData) {
+        if (itemsData.data == undefined) {
+            return (
+                <PageTitle title={"エラー"}></PageTitle>
+            )
+        }
         return (
             <div>
                 <PageTitle title={"商品一覧"}></PageTitle>
-                <OrderSelection goods={data.data!.data} callBack={console.log}></OrderSelection>
+                <OrderSelection goods={itemsData.data!.data} callBack={(ord) => {
+                    setOrder(ord)
+                }}></OrderSelection>
             </div>
         )
 
