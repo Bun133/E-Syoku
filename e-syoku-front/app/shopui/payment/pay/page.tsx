cbf2e40f22e24d1678bafae9f781cdff62b0b9f4
bun133 <bun133.work@gmail.com>
2023-07-09 11:29:12 +0900

    add /shopui/payment/pay

---
+++ e-syoku-front/app/shopui/payment/pay/page.tsx
@@ -0,0 +1,68 @@
+"use client"
+import {useSearchParams} from "next/navigation";
+import PageTitle from "@/components/pageTitle";
+import {Center, Heading, VStack} from "@chakra-ui/layout";
+import {Container, FormControl, FormErrorMessage, FormLabel, Input} from "@chakra-ui/react";
+import {useRef, useState} from "react";
+import Btn from "@/components/btn";
+import {callEndpoint} from "@/lib/e-syoku-api/Axios";
+import {markPaymentPaidEndpoint} from "@/lib/e-syoku-api/EndPoints";
+import {useDisclosure} from "@chakra-ui/hooks";
+import {MessageModal} from "@/components/modal/MessageModal";
+import {useFirebaseAuth} from "@/lib/firebase/authentication";
+
+export default function Page() {
+    const params = useSearchParams()
+    const uid = params.get("uid")
+    const paymentId = params.get("paymentId")
+
+    const [amount, setAmount] = useState(0)
+    const isAmountError = amount <= 0
+
+    const {isOpen, onOpen, onClose} = useDisclosure()
+    const message = useRef([""])
+    const auth = useFirebaseAuth()
+
+    if (!uid || !paymentId) {
+        return (
+            <>
+                <PageTitle title={""}/>
+                <Center>
+                    <Heading>uid,paymentIdを指定してください</Heading>
+                </Center>
+            </>
+        )
+    }
+
+    return (
+        <>
+            <PageTitle title={`決済取扱い:${uid}:${paymentId}`}/>
+            <Container>
+                <VStack>
+                    <FormControl isRequired={true} isInvalid={isAmountError}>
+                        <FormLabel>決済金額</FormLabel>
+                        <Input type={"number"} value={amount} onChange={(e) => setAmount(Number(e.target.value))}/>
+                        {isAmountError ? (<FormErrorMessage>
+                            金額を入力してください
+                        </FormErrorMessage>) : null}
+                    </FormControl>
+                    <Btn onClick={() => {
+                        const f = async () => {
+                            const res = await callEndpoint(markPaymentPaidEndpoint, auth.user, {
+                                paidAmount: amount,
+                                paidMeans: "テスト",
+                                userId: uid,
+                                paymentId: paymentId,
+                                remark: "テストです！"
+                            })
+                            message.current = ["Response:", JSON.stringify(res)]
+                            onOpen()
+                        }
+                        f()
+                    }} disabled={isAmountError}>決済処理</Btn>
+                    <MessageModal message={message.current} isOpen={isOpen} onClose={onClose}></MessageModal>
+                </VStack>
+            </Container>
+        </>
+    )
+}
