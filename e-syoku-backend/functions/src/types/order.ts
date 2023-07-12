e583c3d449047df7992ca0bbd686d894a55b9f42
bun133 <bun133.work@gmail.com>
2023-07-12 16:45:22 +0900

    add singleOrderSchema

--- e-syoku-backend/functions/src/types/order.ts
+++ e-syoku-backend/functions/src/types/order.ts
@@ -3,16 +3,18 @@ import {z} from "zod";
 
 /////// Order(注文内容データ) ////////
 
-/**
- * This type express an order.
- * It is not ensured that the order is valid or paid.
- */
-export const orderSchema = z.array(z.object({
+export const singleOrderSchema = z.object({
     // 商品ID
     goodsId: uniqueId,
     // 注文数
     count: z.number(),
-}))
+})
 
+/**
+ * This type express an order.
+ * It is not ensured that the order is valid or paid.
+ */
+export const orderSchema = z.array(singleOrderSchema)
 
+export type SingleOrder = z.infer<typeof singleOrderSchema>
 export type Order = z.infer<typeof orderSchema>
