6c98cb705898bd80362d4a444a29f105b1ea43a4
bun133 <bun133.work@gmail.com>
2023-07-16 15:55:43 +0900

    rewrite with APIEndpoint Component

--- e-syoku-front/app/order/page.tsx
+++ e-syoku-front/app/order/page.tsx
@@ -1,8 +1,7 @@
 "use client"
-import {callEndpoint, useEndpoint} from "@/lib/e-syoku-api/Axios";
+import {callEndpoint} from "@/lib/e-syoku-api/Axios";
 import {listGoodsEndPoint, submitOrderEndPoint} from "@/lib/e-syoku-api/EndPoints";
 import PageTitle from "@/components/pageTitle";
-import {Loader} from "react-feather";
 import {Center, VStack} from "@chakra-ui/layout";
 import {OrderSelection} from "@/components/form/OrderSelection";
 import {useRef, useState} from "react";
@@ -12,66 +11,55 @@ import Btn from "@/components/btn";
 import {useFirebaseAuth} from "@/lib/firebase/authentication";
 import {useDisclosure} from "@chakra-ui/hooks";
 import {MessageModal} from "@/components/modal/MessageModal";
+import {APIEndpoint} from "@/lib/e-syoku-api/APIEndpointComponent";
 
 export default function Page() {
-    const {response: itemsData} = useEndpoint(listGoodsEndPoint, {})
     const [order, setOrder] = useState<Order>()
     const auth = useFirebaseAuth()
     const {isOpen, onOpen, onClose} = useDisclosure()
     const message = useRef([""])
 
-    if (order && itemsData) {
-        return (
-            <div>
-                <PageTitle title={"注文確定画面"}></PageTitle>
-                <VStack>
-                    {order.map((orderData, index) => {
-                        const item = itemsData.data?.data.find((item) => item.key.goodsId === orderData.goodsId)
-                        if (!item) return null
-                        return (
-                            <Goods key={index} goods={item.key} footer={(<div>
-                                注文個数:{orderData.count}
-                            </div>)}></Goods>
-                        )
-                    })}
+    return (
+        <>
+            {order !== undefined ? <PageTitle title={"注文確定画面"}></PageTitle> :
+                <PageTitle title={"商品一覧"}></PageTitle>}
+            <APIEndpoint endpoint={listGoodsEndPoint} query={{}} onEnd={(response, reload) => {
+                const itemsData = response.data
+                if (order) {
+                    return (
+                        <>
+                            <VStack>
+                                {order.map((orderData, index) => {
+                                    const item = itemsData.data.find((item) => item.key.goodsId === orderData.goodsId)
+                                    if (!item) return null
+                                    return (
+                                        <Goods key={index} goods={item.key} footer={(<div>
+                                            注文個数:{orderData.count}
+                                        </div>)}></Goods>
+                                    )
+                                })}
 
-                    <Center>
-                        <Btn onClick={() => {
-                            callEndpoint(submitOrderEndPoint, auth.user, {order: order}).then((d) => {
-                                message.current = ["Response:", JSON.stringify(d)]
-                                onOpen()
-                            })
-                        }}>注文を確定</Btn>
-                        <MessageModal message={message.current} isOpen={isOpen} onClose={onClose}></MessageModal>
-                    </Center>
-                </VStack>
-            </div>
-        )
-    }
+                                <Center>
+                                    <Btn onClick={() => {
+                                        callEndpoint(submitOrderEndPoint, auth.user, {order: order}).then((d) => {
+                                            message.current = ["Response:", JSON.stringify(d)]
+                                            onOpen()
+                                        })
+                                    }}>注文を確定</Btn>
+                                    <MessageModal message={message.current} isOpen={isOpen}
+                                                  onClose={onClose}></MessageModal>
+                                </Center>
+                            </VStack>
+                        </>
+                    )
+                }
 
-    if (itemsData) {
-        if (itemsData.data == undefined) {
-            return (
-                <PageTitle title={"エラー"}></PageTitle>
-            )
-        }
-        return (
-            <div>
-                <PageTitle title={"商品一覧"}></PageTitle>
-                <OrderSelection goods={itemsData.data!.data} callBack={(ord) => {
-                    setOrder(ord)
-                }}></OrderSelection>
-            </div>
-        )
-
-    } else {
-        return (
-            <div>
-                <PageTitle title={"商品一覧"}></PageTitle>
-                <Center>
-                    <Loader></Loader>
-                </Center>
-            </div>
-        )
-    }
+                return (
+                    <OrderSelection goods={itemsData.data} callBack={(ord) => {
+                        setOrder(ord)
+                    }}></OrderSelection>
+                )
+            }}/>
+        </>
+    )
 }
