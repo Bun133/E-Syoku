c9f96803f73b8a5b8b0d13b229227cb18cd4a645
bun133 <bun133.work@gmail.com>
2023-07-19 18:08:42 +0900

    fix import code

--- e-syoku-backend/functions/src/types/types.ts
+++ e-syoku-backend/functions/src/types/types.ts
@@ -1,7 +1,5 @@
-import {Timestamp} from "firebase-admin/firestore";
+import {DocumentReference, Timestamp} from "firebase-admin/firestore";
 import {z} from "zod";
-import {firestore} from "firebase-admin";
-import DocumentReference = firestore.DocumentReference;
 
 /**
  * Unique ID Type, used for identifying items in the database.
@@ -11,6 +9,4 @@ export const uniqueId = z.string();
 export type UniqueId = z.infer<typeof uniqueId>
 
 export const timeStampSchema = z.instanceof(Timestamp);
-export const firestoreRefSchema = z.object({}).refine((obj)=>{
-    return obj instanceof DocumentReference;
-})
+export const firestoreRefSchema = z.custom<DocumentReference>((t) => t instanceof DocumentReference)
