0930fc60e36eaead1d6cd381c5f3b4fc783a1650
bun133 <bun133.work@gmail.com>
2023-07-19 18:36:24 +0900

    add PageTitle to /shopui/tickets/display

--- e-syoku-front/app/shopui/tickets/display/page.tsx
+++ e-syoku-front/app/shopui/tickets/display/page.tsx
@@ -5,25 +5,29 @@ import {Container} from "@chakra-ui/react";
 import {Center, VStack} from "@chakra-ui/layout";
 import Btn from "@/components/btn";
 import {useRouter} from "next/navigation";
+import PageTitle from "@/components/pageTitle";
 
 export default function Page() {
     const router = useRouter()
     return (
-        <APIEndpoint endpoint={listShopsEndPoint} query={{}} onEnd={(data) => {
-            return (
-                <Container>
-                    <VStack>
-                        <Center>店舗を選んでください</Center>
-                        {data.data.shops.map((shop) => {
-                            return (
-                                <Btn onClick={() => {
-                                    router.push(`/shopui/tickets/display/id?shopId=${shop.shopId}`)
-                                }}>{shop.name}</Btn>
-                            )
-                        })}
-                    </VStack>
-                </Container>
-            )
-        }}/>
+        <>
+            <PageTitle title={"店舗選択"}/>
+            <APIEndpoint endpoint={listShopsEndPoint} query={{}} onEnd={(data) => {
+                return (
+                    <Container>
+                        <VStack>
+                            <Center>店舗を選んでください</Center>
+                            {data.data.shops.map((shop) => {
+                                return (
+                                    <Btn onClick={() => {
+                                        router.push(`/shopui/tickets/display/id?shopId=${shop.shopId}`)
+                                    }}>{shop.name}</Btn>
+                                )
+                            })}
+                        </VStack>
+                    </Container>
+                )
+            }}/>
+        </>
     )
 }
