361f6833fe15828a30d117a576a7bf851a4a84f4
bun133 <bun133.work@gmail.com>
2023-07-29 10:18:10 +0900

    enhanced hangBar

--- e-syoku-front/components/hangBar.tsx
+++ e-syoku-front/components/hangBar.tsx
@@ -1,46 +1,92 @@
 "use client"
-import {UserCheck, UserX} from "react-feather";
+import {Menu, UserCheck, UserX} from "react-feather";
 import {useContext} from "react";
 import {firebaseAuthContext, FirebaseAuthContextType} from "@/lib/firebase/authentication";
 import {signOut} from "@firebase/auth";
 import {useRouter} from "next/navigation";
 import Btn from "@/components/btn";
-import {Flex, HStack} from "@chakra-ui/layout";
-import {Spacer, Text} from "@chakra-ui/react";
-import {AuthState} from "@/lib/e-syoku-api/AuthTypeProvider";
+import {Divider, Flex, HStack, VStack} from "@chakra-ui/layout";
+import {
+    Box,
+    Drawer,
+    DrawerBody,
+    DrawerCloseButton,
+    DrawerContent,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    Text
+} from "@chakra-ui/react";
+import {AdminOnly, AuthState} from "@/lib/e-syoku-api/AuthTypeProvider";
+import {useDisclosure} from "@chakra-ui/hooks";
 
 export function HangBar() {
     const auth = useContext(firebaseAuthContext)
     const router = useRouter()
+    const {isOpen, onOpen, onClose} = useDisclosure()
 
     return (
-        <Flex backgroundColor={"blue.300"} w={"full"} py={2} mb={1}>
-            <HStack>
-                {auth.user !== undefined ? <UserCheck onClick={() => {
-                    router.push("/account")
-                }}></UserCheck> : <UserX></UserX>}
-                <AuthState child={(info) => {
-                    if (info !== undefined && info.authType !== undefined) {
-                        return (<Text>AuthType:{info.authType}</Text>)
-                    } else {
-                        return null
-                    }
-                }}/>
-                <Btn onClick={() => {
-                    logOut(auth)
-                }}>ログアウト</Btn>
-                <Btn href="/auth/register">
-                    本登録
-                </Btn>
-                <Btn href="/auth/login">
-                    ログイン
-                </Btn>
-            </HStack>
-            <Spacer/>
-            <HStack>
-                <a className="text" href="https://github.com/Bun133/E-Syoku" target="_blank">Powered By E-Syoku</a>
-            </HStack>
-        </Flex>
+        <>
+            <Flex w={"full"} shadow={1} backgroundColor={"blue.300"} mb={2}>
+                <Box p={2}>
+                    <Menu onClick={onOpen}/>
+                </Box>
+            </Flex>
+            <Drawer placement={"left"} isOpen={isOpen} onClose={onClose}>
+                <DrawerOverlay/>
+                <DrawerContent>
+                    <DrawerHeader>
+                        <DrawerCloseButton/>
+                    </DrawerHeader>
+                    <DrawerBody>
+                        <VStack my={1}>
+                            <Text>アカウントメニュー</Text>
+                            <Divider/>
+                        </VStack>
+                        <HStack>
+                            {auth.user !== undefined ? <UserCheck onClick={() => {
+                                router.push("/account")
+                            }}></UserCheck> : <UserX></UserX>}
+                            <AuthState children={(info) => {
+                                if (info !== undefined && info.authType !== undefined) {
+                                    return (<Text>AuthType:{info.authType}</Text>)
+                                } else {
+                                    return null
+                                }
+                            }}/>
+                        </HStack>
+                        <VStack>
+                            <Btn onClick={() => {
+                                logOut(auth)
+                                onClose()
+                            }}>ログアウト</Btn>
+                            <Btn href="/auth/register" onClick={onClose}>
+                                本登録
+                            </Btn>
+                            <Btn href="/auth/login" onClick={onClose}>
+                                ログイン
+                            </Btn>
+                        </VStack>
+
+                        <AdminOnly>
+                            <VStack my={1}>
+                                <Text>Adminメニュー</Text>
+                                <Divider/>
+                            </VStack>
+                            <VStack>
+                                <Btn href="/cms" onClick={onClose}>
+                                    CMS
+                                </Btn>
+                            </VStack>
+                        </AdminOnly>
+                    </DrawerBody>
+                    <DrawerFooter>
+                        <a className="text" href="https://github.com/Bun133/E-Syoku" target="_blank">Powered By
+                            E-Syoku</a>
+                    </DrawerFooter>
+                </DrawerContent>
+            </Drawer>
+        </>
     )
 }
 
