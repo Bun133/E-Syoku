0645f5041f7cf175afb2f8ce9728d3f538441bf4
bun133 <bun133.work@gmail.com>
2023-08-01 20:37:48 +0900

    fix APIErrorModal

--- e-syoku-front/components/modal/APIErrorModal.tsx
+++ e-syoku-front/components/modal/APIErrorModal.tsx
@@ -1,6 +1,6 @@
 import {EndPointErrorResponse} from "@/lib/e-syoku-api/Axios";
 import {useDisclosure} from "@chakra-ui/hooks";
-import React, {useRef} from "react";
+import React, {useEffect, useRef} from "react";
 import {MessageModal} from "@/components/modal/MessageModal";
 import {Code} from "@chakra-ui/react";
 
@@ -9,13 +9,16 @@ export function APIErrorModal(params: {
 }) {
     const {isOpen, onOpen, onClose} = useDisclosure()
     const message = useRef<React.ReactNode[]>()
-    if (params.error && !isOpen) {
-        message.current = [<p key={"title"}>エラーが発生しました</p>, <p key={"code"}>エラーコード:{params.error.errorCode}</p>,
-            <p key={"message"}>エラーメッセージ:{params.error.error}</p>, (<div key={"stack"}><p>スタック情報:</p><Code>{params.error.stack}</Code></div>)]
-        onOpen()
-    } else if (!params.error && isOpen) {
-        onClose()
-    }
+
+    useEffect(()=>{
+        if (params.error && !isOpen) {
+            message.current = [<p key={"title"}>エラーが発生しました</p>, <p key={"code"}>エラーコード:{params.error.errorCode}</p>,
+                <p key={"message"}>エラーメッセージ:{params.error.error}</p>, (<div key={"stack"}><p>スタック情報:</p><Code>{params.error.stack}</Code></div>)]
+            onOpen()
+        } else if (!params.error && isOpen) {
+            onClose()
+        }
+    },[params.error])
 
     return (
         <MessageModal message={message.current ?? []} isOpen={isOpen} onClose={onClose}/>
