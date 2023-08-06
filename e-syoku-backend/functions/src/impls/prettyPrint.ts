c6bda4c7d6fd58dfb6f4a959f4faca2b357a8ac6
bun133 <bun133.work@gmail.com>
2023-08-06 14:34:56 +0900

    add PrettyPrints

---
+++ e-syoku-backend/functions/src/impls/prettyPrint.ts
@@ -0,0 +1,138 @@
+import {Ticket, TicketStatus} from "../types/ticket";
+import {DBRefs} from "../utils/db";
+import {
+    PrettyGoods,
+    PrettyOrder,
+    PrettySingleOrder,
+    PrettyTicket,
+    PrettyTicketStatus,
+    PrettyTimeStamp
+} from "../types/prettyPrint";
+import {Timestamp} from "firebase-admin/firestore";
+import {Order, SingleOrder} from "../types/order";
+import {getGoodsById} from "./goods";
+import {SingleError, TypedResult, TypedSingleResult, TypedSuccess} from "../types/errors";
+import {Goods} from "../types/goods";
+import {getShopById} from "./shop";
+import {prettyOrderFailed} from "./errors";
+
+function prettyTimeStamp(timeStamp: Timestamp): PrettyTimeStamp {
+    const date = timeStamp.toDate();
+    return {
+        utcSeconds: date.getTime() / 1000,
+    }
+}
+
+export async function prettyGoods(refs: DBRefs, goods: Goods): Promise<TypedSingleResult<PrettyGoods>> {
+    const shop = await getShopById(refs, goods.shopId)
+    if (!shop.isSuccess) {
+        return shop
+    } else {
+        const suc: TypedSuccess<PrettyGoods> = {
+            isSuccess: true,
+            data: {
+                shop: shop.data,
+                goodsId: goods.goodsId,
+                name: goods.name,
+                price: goods.price,
+                description: goods.description,
+                imageUrl: goods.imageUrl,
+            }
+        }
+
+        return suc
+    }
+}
+
+export async function prettySingleOrder(refs: DBRefs, order: SingleOrder): Promise<TypedSuccess<PrettySingleOrder> | SingleError> {
+    const goodsData = await getGoodsById(refs, order.goodsId)
+    if (!goodsData.isSuccess) {
+        return goodsData
+    } else {
+        const pGoods = await prettyGoods(refs, goodsData.data)
+        if (!pGoods.isSuccess) {
+            return pGoods
+        }
+        const suc: TypedSuccess<PrettySingleOrder> = {
+            isSuccess: true,
+            data: {
+                goods: pGoods.data,
+                count: order.count,
+            }
+        }
+        return suc
+    }
+}
+
+export async function prettyOrder(refs: DBRefs, order: Order): Promise<TypedResult<PrettyOrder>> {
+    const singles = await Promise.all(order.map(single => prettySingleOrder(refs, single)))
+    const errors: SingleError[] = []
+    const successes: TypedSuccess<PrettySingleOrder>[] = []
+
+    for (const single of singles) {
+        if (single.isSuccess) {
+            successes.push(single)
+        } else {
+            errors.push(single)
+        }
+    }
+
+
+    if (errors.length > 0) {
+        const error = prettyOrderFailed(errors)
+        return error
+    } else {
+        const order: PrettySingleOrder[] = successes.map(single => single.data)
+        const suc: TypedSuccess<PrettyOrder> = {
+            isSuccess: true,
+            data: order
+        }
+
+        return suc
+    }
+}
+
+export function prettyStatus(status: TicketStatus): PrettyTicketStatus {
+    switch (status) {
+        case "PROCESSING":
+            return "注文済み"
+        case "COOKING":
+            return "調理中"
+        case "CALLED":
+            return "受け取り待ち"
+        case "RESOLVED":
+            return "完了"
+        case "INFORMED":
+            return "お知らせ"
+    }
+}
+
+export async function prettyTicket(refs: DBRefs, ticket: Ticket): Promise<TypedResult<PrettyTicket>> {
+    const timeStamp = prettyTimeStamp(ticket.issueTime)
+    const order = await prettyOrder(refs, ticket.orderData)
+    if (!order.isSuccess) {
+        return order
+    }
+    const status = prettyStatus(ticket.status)
+    const shop = await getShopById(refs, ticket.shopId)
+    if (!shop.isSuccess) {
+        return shop
+    }
+    const prettyTicket: PrettyTicket = {
+        issueTime: timeStamp,
+        status: status,
+        orderData: order.data,
+        uniqueId: ticket.uniqueId,
+        shop: shop.data,
+        customerId: ticket.customerId,
+        ticketNum: ticket.ticketNum,
+        paymentSessionId: ticket.paymentSessionId,
+    }
+
+    const suc: TypedSuccess<PrettyTicket> = {
+        isSuccess: true,
+        data: prettyTicket
+    }
+
+    return suc
+}
---
+++ e-syoku-backend/functions/src/types/prettyPrint.ts
@@ -0,0 +1,58 @@
+import {z} from "zod";
+import {uniqueId} from "./types";
+import {shopSchema} from "./shop";
+
+export const prettyTicketStatusSchema = z.union([
+    // PROCESSING
+    z.literal("注文済み"),
+    // COOKING
+    z.literal("調理中"),
+    // CALLED
+    z.literal("受け取り待ち"),
+    // RESOLVED
+    z.literal("完了"),
+    // INFORMED
+    z.literal("お知らせ")
+])
+
+export type PrettyTicketStatus = z.infer<typeof prettyTicketStatusSchema>
+
+export const prettyTimeStampSchema = z.object({
+    utcSeconds: z.number(),
+})
+
+export type PrettyTimeStamp = z.infer<typeof prettyTimeStampSchema>
+
+export const prettyGoodsSchema = z.object({
+    goodsId:uniqueId,
+    shop:shopSchema,
+    name:z.string(),
+    price:z.number(),
+    description:z.string().optional(),
+    imageUrl:z.string().optional(),
+})
+
+export type PrettyGoods = z.infer<typeof prettyGoodsSchema>
+
+export const prettySingleOrderSchema = z.object({
+    goods:prettyGoodsSchema,
+    count:z.number()
+})
+
+export type PrettySingleOrder = z.infer<typeof prettySingleOrderSchema>
+
+export const prettyOrderSchema = z.array(prettySingleOrderSchema)
+
+export type PrettyOrder = z.infer<typeof prettyOrderSchema>
+export const prettyTicketSchema = z.object({
+    uniqueId: uniqueId,
+    ticketNum: z.string(),
+    shop: shopSchema,
+    customerId: uniqueId,
+    issueTime: prettyTimeStampSchema,
+    orderData: prettyOrderSchema,
+    paymentSessionId: uniqueId,
+    status: prettyTicketStatusSchema
+})
+
+export type PrettyTicket = z.infer<typeof prettyTicketSchema>
