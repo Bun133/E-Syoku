90b69e29753d7a0a67e33f9dfa41263913f72aa4
bun133 <bun133.work@gmail.com>
2023-08-07 07:32:29 +0900

    changed landing page

--- e-syoku-front/app/page.tsx
+++ e-syoku-front/app/page.tsx
@@ -1,36 +1,24 @@
 "use client"
 import Btn from "@/components/btn";
 import {Center, VStack} from "@chakra-ui/layout";
-import {Box} from "@chakra-ui/react";
+import {Box, Container} from "@chakra-ui/react";
 
 export default function Home() {
     return (
-        <Box minH={"max-content"}>
+        <Container>
             <Center>
                 <VStack>
-                    <Btn href="/tickets">
-                        食券一覧
-                    </Btn>
-                    <Btn href="/shopui/tickets/display">
-                        食券一覧画面(店舗側)
-                    </Btn>
-                    <Btn href="/shopui/tickets/call">
-                        食券呼び出し(店舗側)
-                    </Btn>
-                    <Btn href="/order/">
-                        メニュー
+                    <Btn href="/order">
+                        新規注文
                     </Btn>
                     <Btn href="/payment/">
                         決済一覧
                     </Btn>
-                    <Btn href="/shopui/payment/scan">
-                        決済処理取り扱い(店舗側)
-                    </Btn>
-                    <Btn href="/account/grant">
-                        権限付与
+                    <Btn href="/tickets">
+                        食券一覧
                     </Btn>
                 </VStack>
             </Center>
-        </Box>
+        </Container>
     )
 }
