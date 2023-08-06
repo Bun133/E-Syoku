2c0ee5300dbc627162080c9c60b0250f555a400d
bun133 <bun133.work@gmail.com>
2023-08-06 20:49:12 +0900

    enhanced orderDataTransform

--- e-syoku-front/lib/e-syoku-api/Transformers.ts
+++ e-syoku-front/lib/e-syoku-api/Transformers.ts
@@ -1,5 +1,5 @@
 import {PrettyOrder, TicketStatus} from "@/lib/e-syoku-api/Types";
 
 export function orderDataTransform(orderData: PrettyOrder) {
-    return orderData.map(order => `[${order.goods.name} ${order.count}個]`).join(",")
+    return orderData.map(order => `[${order.goods.name}×${order.count}個]`).join(",")
 }
