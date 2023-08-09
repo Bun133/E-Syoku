9ea534de22ef9dfc5c6d541e312aec4a3cc0b307
bun133 <bun133.work@gmail.com>
2023-08-09 17:54:04 +0900

    add TicketDisplay Component

--- e-syoku-front/app/shopui/tickets/display/id/page.tsx
+++ e-syoku-front/app/shopui/tickets/display/id/page.tsx
@@ -1,13 +1,11 @@
 "use client"
 import {APIEndpoint} from "@/lib/e-syoku-api/APIEndpointComponent";
 import {ticketDisplayEndpoint} from "@/lib/e-syoku-api/EndPoints";
-import {Center, Flex, Heading, HStack, VStack} from "@chakra-ui/layout";
+import {Center, Heading, VStack} from "@chakra-ui/layout";
 import Btn from "@/components/btn";
 import {useSearchParams} from "next/navigation";
 import PageTitle from "@/components/pageTitle";
-import {Box} from "@chakra-ui/react";
-import {useEffect, useRef} from "react";
-import {ticketColor} from "@/components/Ticket";
+import {TicketDisplay} from "@/components/TicketDisplay";
 
 export default function Page() {
     const params = useSearchParams()
@@ -18,25 +16,10 @@ export default function Page() {
             <PageTitle title={"TicketDisplay"}/>
             <APIEndpoint endpoint={ticketDisplayEndpoint} query={{shopId: shopId}}
                          onEnd={(response, reload) => {
-                             const displays = response.data.displays.sort((a, b) => {
-                                 return a.lastUpdated._seconds - b.lastUpdated._seconds
-                             })
-
-                             const processing = displays.filter((display) => display.status === "PROCESSING")
-                             const called = displays.filter((display) => display.status === "CALLED")
-                             const informed = displays.filter((display) => display.status === "INFORMED")
-                             const resolved = displays.filter((display) => display.status === "RESOLVED")
-
 
                              return (
                                  <VStack>
-                                     <TicketDisplay displays={processing} ticketStatus={"調理中"}
-                                                    ticketColor={ticketColor("調理中")} autoScroll={true}/>
-                                     <TicketDisplay displays={called} ticketStatus={"受け渡し可能"}
-                                                    ticketColor={ticketColor("受け取り待ち")} autoScroll={false}/>
-                                     {informed.length > 0 && (
-                                         <TicketDisplay displays={informed} ticketStatus={"お呼び出し"}
-                                                        ticketColor={ticketColor("お知らせ")} autoScroll={true}/>)}
+                                     <TicketDisplay data={response.data.displays}/>
                                      <Btn onClick={reload}>再読み込み</Btn>
                                  </VStack>
                              )
@@ -50,64 +33,5 @@ export default function Page() {
                 )
             }}/>
         </>
-    )
-}
-
-function TicketDisplay(props: {
-    displays: { ticketId: string, ticketNum: string }[],
-    ticketStatus: string,
-    ticketColor: string,
-    autoScroll: boolean
-}) {
-    const duration = 2000
-    const currentIndex = useRef(0)
-    const listElement = useRef<HTMLDivElement>(null)
-
-    function moveToElement(elementIndex: number) {
-        if (listElement.current == null) return
-        const element = listElement.current.children[elementIndex]
-        if (element == null) return
-        element.scrollIntoView({
-            behavior: "smooth",
-        })
-    }
-
-    function move() {
-        if (listElement.current != null) {
-            const len = listElement.current.children.length
-            let toBeIndex = currentIndex.current + 1
-            if (toBeIndex >= len) {
-                toBeIndex = 0
-            }
-
-            moveToElement(toBeIndex)
-            currentIndex.current = toBeIndex
-        }
-        setTimeout(move, duration)
-    }
-
-    useEffect(() => {
-        if (props.autoScroll) {
-            move()
-        }
-    }, [])
-
-    return (
-        <Flex direction={"column"} alignContent={"start"} w={"100%"}>
-            <Heading>{props.ticketStatus}</Heading>
-            <Box h={"1rem"}/>
-            <HStack spacing={"2rem"} overflowX={"scroll"} w={"100%"} px={2} ref={listElement}>
-                {props.displays.map((display) => {
-                    return (
-                        <Box key={display.ticketId} backgroundColor={props.ticketColor} w={"8rem"} p={2}
-                             borderRadius={5} flexShrink={0} flexGrow={0}>
-                            <Center>
-                                <Heading>{display.ticketNum}</Heading>
-                            </Center>
-                        </Box>
-                    )
-                })}
-            </HStack>
-        </Flex>
     )
 }
---
+++ e-syoku-front/components/TicketDisplay.tsx
@@ -0,0 +1,96 @@
+import {TicketDisplayData} from "@/lib/e-syoku-api/Types";
+import {Center, Flex, Heading, HStack, VStack} from "@chakra-ui/layout";
+import {useEffect, useRef} from "react";
+import {Box} from "@chakra-ui/react";
+import {ticketColor} from "@/components/Ticket";
+
+export type DisplaySelection = {
+    processing: boolean,
+    called: boolean,
+    informed: boolean,
+    resolved: boolean,
+}
+
+const defaultDisplaySelection: DisplaySelection = {
+    processing: true,
+    called: true,
+    informed: false,
+    resolved: false,
+}
+
+export function TicketDisplay(params: {
+    data: TicketDisplayData[],
+    displaySelection?: DisplaySelection
+}) {
+    const selection = params.displaySelection ?? defaultDisplaySelection
+    const sorted = params.data.sort((a, b) => a.lastUpdated._seconds - b.lastUpdated._seconds)
+    const processing = sorted.filter(e => e.status === "PROCESSING")
+    const called = sorted.filter(e => e.status === "CALLED")
+    const informed = sorted.filter(e => e.status === "INFORMED")
+    const resolved = sorted.filter(e => e.status === "RESOLVED")
+
+    return (
+        <VStack w={"100%"}>
+            {selection.processing &&
+                <TicketDisplayRow title={"調理中"} displays={processing} ticketColor={ticketColor("調理中")}/>}
+            {selection.resolved &&
+                <TicketDisplayRow title={"受け渡し可能"} displays={called} ticketColor={ticketColor("受け取り待ち")}/>}
+            {selection.informed &&
+                <TicketDisplayRow title={"お呼び出し"} displays={informed} ticketColor={ticketColor("お知らせ")}/>}
+            {selection.resolved &&
+                <TicketDisplayRow title={"完了"} displays={resolved} ticketColor={ticketColor("完了")}/>}
+        </VStack>
+    )
+}
+
+function TicketDisplayRow(props: { title: string, displays: TicketDisplayData[], ticketColor: string }) {
+    const duration = 2000
+    const currentIndex = useRef(0)
+    const listElement = useRef<HTMLDivElement>(null)
+
+    function moveToElement(elementIndex: number) {
+        if (listElement.current == null) return
+        const element = listElement.current.children[elementIndex]
+        if (element == null) return
+        element.scrollIntoView({
+            behavior: "smooth",
+        })
+    }
+
+    function move() {
+        if (listElement.current != null) {
+            const len = listElement.current.children.length
+            let toBeIndex = currentIndex.current + 1
+            if (toBeIndex >= len) {
+                toBeIndex = 0
+            }
+
+            moveToElement(toBeIndex)
+            currentIndex.current = toBeIndex
+        }
+        setTimeout(move, duration)
+    }
+
+    useEffect(() => {
+        move()
+    }, [])
+
+    return (
+        <Flex direction={"column"} alignContent={"start"} w={"100%"}>
+            <Heading>{props.title}</Heading>
+            <Box h={"1rem"}/>
+            <HStack spacing={"2rem"} overflowX={"scroll"} w={"100%"} px={2} ref={listElement}>
+                {props.displays.map((display) => {
+                    return (
+                        <Box key={display.ticketId} backgroundColor={props.ticketColor} w={"8rem"} p={2}
+                             borderRadius={5} flexShrink={0} flexGrow={0}>
+                            <Center>
+                                <Heading>{display.ticketNum}</Heading>
+                            </Center>
+                        </Box>
+                    )
+                })}
+            </HStack>
+        </Flex>
+    )
+}
