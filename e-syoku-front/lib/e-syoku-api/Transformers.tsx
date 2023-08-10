71afd1366dd3754a7a6d74b7b4823ab0b011aa0e
bun133 <bun133.work@gmail.com>
2023-08-10 22:11:00 +0900

    fix Transformer

--- e-syoku-front/lib/e-syoku-api/Transformers.tsx
+++ e-syoku-front/lib/e-syoku-api/Transformers.tsx
@@ -2,9 +2,9 @@ import {PrettyOrder, TicketStatus} from "@/lib/e-syoku-api/Types";
 import {ListItem} from "@chakra-ui/react";
 
 export function orderDataTransform(orderData: PrettyOrder) {
-    return orderData.map(order => {
+    return orderData.map((order,index) => {
         return (
-            <ListItem>
+            <ListItem key={index}>
                 {order.goods.name}×{order.count}個
             </ListItem>
         )
