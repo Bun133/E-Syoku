7f2f9664deea136a19e7ec1a920cc28efd9cad6a
bun133 <bun133.work@gmail.com>
2023-08-12 17:21:49 +0900

    fix /shopui/payment/scan

--- e-syoku-front/app/shopui/payment/scan/page.tsx
+++ e-syoku-front/app/shopui/payment/scan/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 import PageTitle from "@/components/pageTitle";
-import {Html5QrcodeResult} from "html5-qrcode/es2015/core";
 import {VStack} from "@chakra-ui/layout";
 import {useRouter} from "next/navigation";
 import {BarcodeReader} from "@/components/reader/BarcodeReader";
@@ -17,17 +16,4 @@ export default function Page() {
             </VStack>
         </>
     )
-}
-
-function onScan(decodedText: string, result: Html5QrcodeResult): { userId: string; paymentId: string } | undefined {
-    console.log(decodedText, result)
-    const split = decodedText.split(":")
-    if (split.length == 2) {
-        const userId = split[0]
-        const paymentId = split[1]
-        return {
-            userId, paymentId
-        }
-    }
-    return undefined
 }
