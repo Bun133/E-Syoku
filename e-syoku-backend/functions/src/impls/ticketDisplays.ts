ccb518bd871276a5136bff0977dcdc25b3a2824b
bun133 <bun133.work@gmail.com>
2023-08-13 17:14:56 +0900

    remove ticketDisplayDataByTicketId

--- e-syoku-backend/functions/src/impls/ticketDisplays.ts
+++ e-syoku-backend/functions/src/impls/ticketDisplays.ts
@@ -1,11 +1,5 @@
-import {DBRefs, parseData, parseDataAll} from "../utils/db";
+import {DBRefs, parseDataAll} from "../utils/db";
 import {TicketDisplayData, ticketDisplaySchema} from "../types/ticketDisplays";
-import {TypedSingleResult} from "../types/errors";
-import {dbNotFoundError} from "./errors";
-
-export async function ticketDisplayDataByTicketId(ref: DBRefs, shopId: string, ticketId: string): Promise<TypedSingleResult<TicketDisplayData>> {
-    return parseData(dbNotFoundError("ticketDisplayData"), ticketDisplaySchema, ref.ticketDisplays(shopId).doc(ticketId))
-}
 
 export async function ticketDisplayDataByShopId(ref: DBRefs, shopId: string): Promise<TicketDisplayData[]> {
     return parseDataAll(ticketDisplaySchema, ref.ticketDisplays(shopId))
