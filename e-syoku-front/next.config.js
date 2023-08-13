8f8622373ac01fcaf9b29461c67e76d80c37fda7
bun133 <bun133.work@gmail.com>
2023-08-13 18:01:02 +0900

    add Analyzer

--- e-syoku-front/next.config.js
+++ e-syoku-front/next.config.js
@@ -1,9 +1,14 @@
 const withPWA = require('next-pwa')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+})
 module.exports = withPWA({
     dest: "public",
     register: true,
     skipWaiting: true,
-})({
-    distDir: "firebase_hosting",
-    output: "export"
-})
+})(
+    withBundleAnalyzer({
+        distDir: "firebase_hosting",
+        output: "export"
+    })
+)
