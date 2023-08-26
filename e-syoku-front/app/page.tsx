e38c4024d4e56865602177803ac4b0f0bbc7fece
bun133 <bun133.work@gmail.com>
2023-08-26 20:32:58 +0900

    enhanced /

--- e-syoku-front/app/page.tsx
+++ e-syoku-front/app/page.tsx
@@ -1,38 +1,40 @@
 "use client"
-import Btn from "@/components/btn";
-import {Center, VStack} from "@chakra-ui/layout";
-import {Box, Container} from "@chakra-ui/react";
+import {Center} from "@chakra-ui/layout";
 import {CashierOnly, ShopOnly} from "@/lib/e-syoku-api/AuthTypeProvider";
+import {Box, Flex, Spacer, Text} from "@chakra-ui/react";
+import Link from "next/link";
 
 export default function Home() {
     return (
-        <Container>
-            <Center>
-                <VStack>
-                    <Btn href="/buy">
-                        購入(新)
-                    </Btn>
-                    <Btn href="/order">
-                        新規注文
-                    </Btn>
-                    <Btn href="/payment/">
-                        支払い一覧
-                    </Btn>
-                    <Btn href="/tickets">
-                        食券一覧
-                    </Btn>
-                    <CashierOnly>
-                        <Btn href="/shopui/payment/scan">
-                            決済取扱
-                        </Btn>
-                    </CashierOnly>
-                    <ShopOnly>
-                        <Btn href="/shopui/tickets/call">
-                            食券呼び出し
-                        </Btn>
-                    </ShopOnly>
-                </VStack>
-            </Center>
-        </Container>
+        <Center h={"calc(100vh - 1rem - 24px - 8px)"} w={"full"} p={2}>
+            <Flex h={"full"} w={"67%"} direction={"column"} alignItems={"center"}>
+                <LinkBtn href="/buy" text="新規購入"/>
+                <Spacer/>
+                <LinkBtn href="/payment" text="支払い一覧"/>
+                <Spacer/>
+                <LinkBtn href="/tickets" text="食券一覧"/>
+                <CashierOnly>
+                    <Spacer/>
+                    <LinkBtn href="/shopui/payment/scan" text="決済取扱"/>
+                </CashierOnly>
+                <ShopOnly>
+                    <Spacer/>
+                    <LinkBtn href="/shopui/tickets/call" text="食券呼び出し"/>
+                </ShopOnly>
+            </Flex>
+        </Center>
+    )
+}
+
+function LinkBtn(params: { href: string, text: string }) {
+    return (
+        <Box w={"full"} mx={3} backgroundColor={"blue.500"} borderRadius={"xl"} boxShadow={"xl"} flexGrow={1}
+             flexShrink={1}>
+            <Link href={params.href} style={{width: "100%", height: "100%"}}>
+                <Center w={"full"} h={"full"}>
+                    <Text fontSize={"3xl"} color={"white"}>{params.text}</Text>
+                </Center>
+            </Link>
+        </Box>
     )
 }
