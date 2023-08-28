5c48a38ca5ffdcf51851ca7dc8e2c9e8ffd9d354
bun133 <bun133.work@gmail.com>
2023-08-28 16:37:16 +0900

    rearranged hangbar drawer

--- e-syoku-front/components/hangBar.tsx
+++ e-syoku-front/components/hangBar.tsx
@@ -41,7 +41,6 @@ function HangEntity(params: { text: string, onClick?: () => void, href?: string 
 
 export function HangBar() {
     const auth = useContext(firebaseAuthContext)
-    const router = useRouter()
     const {isOpen, onOpen, onClose} = useDisclosure()
 
     return (
@@ -64,71 +63,81 @@ export function HangBar() {
                         <DrawerCloseButton/>
                     </DrawerHeader>
                     <DrawerBody>
-                        <VStack my={1}>
-                            <Text>メニュー</Text>
-                            <Divider/>
-                        </VStack>
-                        <VStack>
-                            <HangEntity text={"ホーム"} href="/" onClick={onClose}/>
-                            <HangEntity text={"新規注文"} href="/order" onClick={onClose}/>
-                            <HangEntity text={"食券一覧"} href="/tickets" onClick={onClose}/>
-                            <HangEntity text={"決済一覧"} href="/payment" onClick={onClose}/>
-                        </VStack>
-
-                        <VStack my={1}>
-                            <Text>アカウントメニュー</Text>
-                            <Divider/>
-                        </VStack>
-                        <HStack>
-                            {auth.user !== undefined ? <UserCheck onClick={() => {
-                                router.push("/account")
-                            }}></UserCheck> : <UserX></UserX>}
-                            <AuthState comp={(info) => {
-                                if (info !== undefined && info.authType !== undefined) {
-                                    return (<Text>AuthType:{info.authType}</Text>)
-                                } else {
-                                    return null
-                                }
-                            }}/>
-                        </HStack>
-                        <VStack>
-                            <HangEntity text={"ログアウト"} onClick={() => {
-                                logOut(auth)
-                                onClose()
-                            }}/>
-                            <HangEntity text={"本登録"} href="/auth/register" onClick={onClose}/>
-                            <HangEntity text={"ログイン"} href="/auth/login" onClick={onClose}/>
-                        </VStack>
-
-                        <ShopOnly>
-                            <VStack my={1}>
-                                <Text>Shopメニュー</Text>
+                        <VStack mb={"1rem"}>
+                            <VStack my={1} w={"full"}>
+                                <Text>メニュー</Text>
                                 <Divider/>
                             </VStack>
                             <VStack>
-                                <HangEntity text={"食券呼び出し"} href="/shopui/tickets/call" onClick={onClose}/>
-                                <HangEntity text={"食券一覧画面"} href="/shopui/tickets/display" onClick={onClose}/>
+                                <HangEntity text={"ホーム"} href="/" onClick={onClose}/>
+                                <HangEntity text={"新規注文"} href="/order" onClick={onClose}/>
+                                <HangEntity text={"食券一覧"} href="/tickets" onClick={onClose}/>
+                                <HangEntity text={"決済一覧"} href="/payment" onClick={onClose}/>
+                            </VStack>
+                        </VStack>
+
+
+                        <VStack mb={"1rem"}>
+                            <VStack my={1} w={"full"}>
+                                <Text>アカウントメニュー</Text>
+                                <Divider/>
+                            </VStack>
+                            <HStack>
+                                {auth.user !== undefined ? <UserCheck/> : <UserX/>}
+                                <AuthState comp={(info) => {
+                                    if (info !== undefined && info.authType !== undefined) {
+                                        return (<Text>AuthType:{info.authType}</Text>)
+                                    } else {
+                                        return null
+                                    }
+                                }}/>
+                            </HStack>
+                            <VStack>
+                                <HangEntity text={"アカウント詳細"} href="/account" onClick={onClose}/>
+                                <HangEntity text={"ログアウト"} onClick={() => {
+                                    logOut(auth)
+                                    onClose()
+                                }}/>
+                                <HangEntity text={"本登録"} href="/auth/register" onClick={onClose}/>
+                                <HangEntity text={"ログイン"} href="/auth/login" onClick={onClose}/>
+                            </VStack>
+                        </VStack>
+
+                        <ShopOnly>
+                            <VStack mb={"1rem"}>
+                                <VStack my={1} w={"full"}>
+                                    <Text>Shopメニュー</Text>
+                                    <Divider/>
+                                </VStack>
+                                <VStack>
+                                    <HangEntity text={"食券呼び出し"} href="/shopui/tickets/call" onClick={onClose}/>
+                                    <HangEntity text={"食券一覧画面"} href="/shopui/tickets/display" onClick={onClose}/>
+                                </VStack>
                             </VStack>
                         </ShopOnly>
 
                         <CashierOnly>
-                            <VStack my={1}>
-                                <Text>Cashierメニュー</Text>
-                                <Divider/>
-                            </VStack>
-                            <VStack>
-                                <HangEntity text={"決済取扱い"} href="/shopui/payment/scan" onClick={onClose}/>
+                            <VStack mb={"1rem"}>
+                                <VStack my={1} w={"full"}>
+                                    <Text>Cashierメニュー</Text>
+                                    <Divider/>
+                                </VStack>
+                                <VStack>
+                                    <HangEntity text={"決済取扱い"} href="/shopui/payment/scan" onClick={onClose}/>
+                                </VStack>
                             </VStack>
                         </CashierOnly>
 
                         <AdminOnly>
-                            <VStack my={1}>
-                                <Text>Adminメニュー</Text>
-                                <Divider/>
-                            </VStack>
-                            <VStack>
-                                <HangEntity text={"CMS"} href="/cms" onClick={onClose}/>
-                                <HangEntity text={"権限付与"} href="/account/grant" onClick={onClose}/>
+                            <VStack mb={"1rem"}>
+                                <VStack my={1} w={"full"}>
+                                    <Text>Adminメニュー</Text>
+                                    <Divider/>
+                                </VStack>
+                                <VStack>
+                                    <HangEntity text={"CMS"} href="/cms" onClick={onClose}/>
+                                    <HangEntity text={"権限付与"} href="/account/grant" onClick={onClose}/>
+                                </VStack>
                             </VStack>
                         </AdminOnly>
                     </DrawerBody>
