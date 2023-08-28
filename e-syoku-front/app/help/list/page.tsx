cf40ef2c5f5a07eff765d1e7790c955e99bdd4b9
bun133 <bun133.work@gmail.com>
2023-08-28 21:38:52 +0900

    add Shop/Cashier HelpEntry

--- e-syoku-front/app/help/list/page.tsx
+++ e-syoku-front/app/help/list/page.tsx
@@ -1,31 +1,54 @@
 "use client"
-import {allHelpEntries} from "@/lib/e-syoku-api/help/HelpEntries";
-import {Box, Spacer, Text, VStack} from "@chakra-ui/react";
+import {allHelpEntries, HelpEntry} from "@/lib/e-syoku-api/help/HelpEntries";
+import {Badge, Box, Spacer, Text, VStack} from "@chakra-ui/react";
 import {Card, CardBody, CardFooter} from "@chakra-ui/card";
 import Btn from "@/components/btn";
 import {HStack} from "@chakra-ui/layout";
+import {CashierOnly, ShopOnly} from "@/lib/e-syoku-api/AuthTypeProvider";
 
 export default function Page() {
     const entries = allHelpEntries()
     const helpEntries = entries.filter(e => e.displayName !== undefined)
+    const shopEntries = helpEntries.filter(e => e.isToShop)
+    const cashierEntries = helpEntries.filter(e => e.isToCashier)
+    const normalEntries = helpEntries.filter(e => !e.isToShop && !e.isToCashier)
 
     return (
         <VStack w={"full"}>
-            {helpEntries.map((entry, index) => (
-                <Box p={2} m={2} w={"full"} key={index}>
-                    <Card>
-                        <CardBody>
-                            <Text>{entry.displayName}</Text>
-                        </CardBody>
-                        <CardFooter>
-                            <HStack w={"full"}>
-                                <Spacer/>
-                                <Btn href={`/help?fs=${entry.mdFileName}`}>開く</Btn>
-                            </HStack>
-                        </CardFooter>
-                    </Card>
-                </Box>
+            <ShopOnly>
+                {shopEntries.map((entry, index) => (
+                    <HelpComponent key={index} help={entry} isShop={true}/>
+                ))}
+            </ShopOnly>
+            <CashierOnly>
+                {cashierEntries.map((entry, index) => (
+                    <HelpComponent key={index} help={entry} isCashier={true}/>
+                ))}
+            </CashierOnly>
+            {normalEntries.map((entry, index) => (
+                <HelpComponent key={index} help={entry}/>
             ))}
         </VStack>
+    )
+}
+
+function HelpComponent(params: { help: HelpEntry, isShop?: boolean, isCashier?: boolean }) {
+    const entry = params.help
+    return (
+        <Box p={2} m={2} w={"full"}>
+            <Card>
+                <CardBody>
+                    {(params.isShop ?? false) && <Badge colorScheme={"green"}>店舗</Badge>}
+                    {(params.isCashier ?? false) && <Badge colorScheme={"blue"}>決済担当</Badge>}
+                    <Text>{entry.displayName}</Text>
+                </CardBody>
+                <CardFooter>
+                    <HStack w={"full"}>
+                        <Spacer/>
+                        <Btn href={`/help?fs=${entry.mdFileName}`}>開く</Btn>
+                    </HStack>
+                </CardFooter>
+            </Card>
+        </Box>
     )
 }
--- e-syoku-front/lib/e-syoku-api/help/HelpEntries.ts
+++ e-syoku-front/lib/e-syoku-api/help/HelpEntries.ts
@@ -2,6 +2,10 @@ export type HelpEntry = {
     mdFileName: string,
     // Help一覧に出すときの表示名(省略すると非表示)
     displayName?: string,
+    // 店舗担当者用マニュアル
+    isToShop?: boolean,
+    // 決済担当者用マニュアル
+    isToCashier?: boolean,
     validator: (errorCode: string) => boolean
 }
 
@@ -20,6 +24,24 @@ function hintMd(mdName: string, displayName: string): HelpEntry {
     }
 }
 
+function shopMd(mdName: string, displayName: string): HelpEntry {
+    return {
+        mdFileName: mdName,
+        displayName,
+        isToShop: true,
+        validator: (errorCode) => false,
+    }
+}
+
+function cashierMd(mdName: string, displayName: string): HelpEntry {
+    return {
+        mdFileName: mdName,
+        displayName,
+        isToCashier: true,
+        validator: (errorCode) => false,
+    }
+}
+
 function unknownMd(): HelpEntry {
     return {
         mdFileName: "unknown.md",
@@ -32,9 +54,13 @@ const helpEntries: HelpEntry[] = [
     errorMd("INPUT_WRONG_PAIDAMOUNT"),
     errorMd("INPUT_WRONG_BARCODE"),
     errorMd("TICKET_STATUS_INVALID"),
+    errorMd("INTERNAL_BARCODE_BIND_NOT_FOUND"),
     hintMd("HowToOrder.md", "注文方法について"),
     hintMd("HowToCheckTicket.md", "食券を確認したい"),
     hintMd("HowToPay.md", "支払方法について"),
+    shopMd("HowToProvideItems.md","商品受け渡しについて"),
+    cashierMd("HowToReceiveMoney.md","代金受け取りについて"),
+    cashierMd("HowToBindTicket.md","食券の紐づけについて"),
 ]
 
 export function findHelpEntry(errorCode: string): HelpEntry {
