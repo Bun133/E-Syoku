24d45e9979ab10c835021320a6b2b9307a8de725
bun133 <bun133.work@gmail.com>
2023-08-28 16:12:48 +0900

    add displayName

--- e-syoku-front/app/help/list/page.tsx
+++ e-syoku-front/app/help/list/page.tsx
@@ -1,22 +1,27 @@
 "use client"
 import {allHelpEntries} from "@/lib/e-syoku-api/help/HelpEntries";
-import {Box, Text, VStack} from "@chakra-ui/react";
+import {Box, Spacer, Text, VStack} from "@chakra-ui/react";
 import {Card, CardBody, CardFooter} from "@chakra-ui/card";
 import Btn from "@/components/btn";
+import {HStack} from "@chakra-ui/layout";
 
 export default function Page() {
     const entries = allHelpEntries()
+    const helpEntries = entries.filter(e => e.displayName !== undefined)
 
     return (
         <VStack w={"full"}>
-            {entries.map((entry, index) => (
+            {helpEntries.map((entry, index) => (
                 <Box p={2} m={2} w={"full"} key={index}>
                     <Card>
                         <CardBody>
-                            <Text>{entry.mdFileName}</Text>
+                            <Text>{entry.displayName}</Text>
                         </CardBody>
                         <CardFooter>
-                            <Btn href={`/help?fs=${entry.mdFileName}`}>開く</Btn>
+                            <HStack w={"full"}>
+                                <Spacer/>
+                                <Btn href={`/help?fs=${entry.mdFileName}`}>開く</Btn>
+                            </HStack>
                         </CardFooter>
                     </Card>
                 </Box>
