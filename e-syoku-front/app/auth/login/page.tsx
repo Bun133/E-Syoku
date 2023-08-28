4e5c6bdea42b74d3dac278d24a4b2bcb313b7bbc
bun133 <bun133.work@gmail.com>
2023-08-28 16:29:34 +0900

    add waring at /auth/login

--- e-syoku-front/app/auth/login/page.tsx
+++ e-syoku-front/app/auth/login/page.tsx
@@ -4,8 +4,9 @@ import PageTitle from "@/components/pageTitle";
 import Btn from "@/components/btn";
 import {useFirebaseAuth} from "@/lib/firebase/authentication";
 import {Auth, GoogleAuthProvider, signInWithPopup} from "@firebase/auth";
-import {Loader} from "react-feather";
-import {Center} from "@chakra-ui/layout";
+import {AlertTriangle, Loader} from "react-feather";
+import {Center, HStack} from "@chakra-ui/layout";
+import {Box, Text} from "@chakra-ui/react";
 
 export default function Page() {
     const context = useFirebaseAuth()
@@ -24,7 +25,21 @@ export default function Page() {
     return (
         <div>
             <PageTitle title={"ログイン"}></PageTitle>
-            <Center>
+            <Box px={2} mx={2} borderWidth={2} borderRadius={8} borderColor={"orange.300"} bgColor={"orange.100"}>
+                <HStack>
+                    <AlertTriangle color={"orange"} size={36}/>
+                    <Text fontSize={"3xl"}>
+                        注意
+                    </Text>
+                </HStack>
+                <Text>
+                    ログインすると、今までの注文履歴・購入履歴が消えます！
+                </Text>
+                <Text>
+                    注文履歴・購入履歴が消えても自己責任となります。
+                </Text>
+            </Box>
+            <Center p={2}>
                 <Btn onClick={async () => {
                     const r = await loginWithGoogle(auth)
                     console.log("Login result", r)
