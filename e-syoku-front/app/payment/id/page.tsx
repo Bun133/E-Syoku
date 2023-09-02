38326f91386860a0742c37290815f33e31db5cde
bun133 <bun133.work@gmail.com>
2023-09-02 21:11:51 +0900

    enhanced /payment/id

--- e-syoku-front/app/payment/id/page.tsx
+++ e-syoku-front/app/payment/id/page.tsx
@@ -7,6 +7,8 @@ import Btn from "@/components/btn";
 import React from "react";
 import {APIEndpoint} from "@/lib/e-syoku-api/APIEndpointComponent";
 import {PaymentCard} from "@/components/Payment";
+import {WarningBox} from "@/components/messageBox/WarningBox";
+import {Text} from "@chakra-ui/react";
 
 export default function Page() {
     const params = useSearchParams()
@@ -20,7 +22,17 @@ export default function Page() {
                 return (
                     <Center>
                         <VStack>
-                            <PaymentCard payment={response.data.payment}/>
+                            {payment.state === "未支払い" && (
+                                <WarningBox>
+                                    <Text>
+                                        決済前のため、商品は確保されていません
+                                    </Text>
+                                    <Text>
+                                        店頭にて決済をお願いします
+                                    </Text>
+                                </WarningBox>
+                            )}
+                            <PaymentCard payment={payment}/>
                             <Btn onClick={reload}>再読み込み</Btn>
                         </VStack>
                     </Center>
