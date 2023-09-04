4c47e97aa6d044024efe68f875b16bcc06724677
bun133 <bun133.work@gmail.com>
2023-09-04 22:18:44 +0900

    enhanced Login Page

--- e-syoku-front/app/auth/login/page.tsx
+++ e-syoku-front/app/auth/login/page.tsx
@@ -4,13 +4,18 @@ import PageTitle from "@/components/pageTitle";
 import Btn from "@/components/btn";
 import {useFirebaseAuth} from "@/lib/firebase/authentication";
 import {Auth, GoogleAuthProvider, signInWithPopup} from "@firebase/auth";
-import {AlertTriangle, Loader} from "react-feather";
-import {Center, HStack} from "@chakra-ui/layout";
-import {Box, Text} from "@chakra-ui/react";
+import {Loader} from "react-feather";
+import {Center} from "@chakra-ui/layout";
+import {Text} from "@chakra-ui/react";
+import {InfoBox} from "@/components/messageBox/InfoBox";
+import {useRouter} from "next/navigation";
 
 export default function Page() {
     const context = useFirebaseAuth()
     const auth = context.auth
+    const router = useRouter()
+
+
     if (!auth) {
         return (
             <div>
@@ -25,24 +30,18 @@ export default function Page() {
     return (
         <div>
             <PageTitle title={"ログイン"}></PageTitle>
-            <Box px={2} mx={2} borderWidth={2} borderRadius={8} borderColor={"orange.300"} bgColor={"orange.100"}>
-                <HStack>
-                    <AlertTriangle color={"orange"} size={36}/>
-                    <Text fontSize={"3xl"}>
-                        注意
-                    </Text>
-                </HStack>
+            <InfoBox>
                 <Text>
-                    ログインすると、今までの注文履歴・購入履歴が消えます！
+                    サービスのご利用にはログインが必要です。
                 </Text>
                 <Text>
-                    注文履歴・購入履歴が消えても自己責任となります。
+                    ログインが完了すると、自動的に遷移します。
                 </Text>
-            </Box>
+            </InfoBox>
             <Center p={2}>
                 <Btn onClick={async () => {
-                    const r = await loginWithGoogle(auth)
-                    console.log("Login result", r)
+                    await loginWithGoogle(auth)
+                    router.push("/")
                 }}>Googleでログイン</Btn>
             </Center>
         </div>
