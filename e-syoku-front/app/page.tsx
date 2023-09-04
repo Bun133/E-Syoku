ef42fcd5ca16b31d11d2629ee06d8587fba598f4
bun133 <bun133.work@gmail.com>
2023-09-04 22:18:53 +0900

    トップページを開いたとき、未ログインはログインページへ遷移するように

--- e-syoku-front/app/page.tsx
+++ e-syoku-front/app/page.tsx
@@ -3,12 +3,23 @@ import {Center} from "@chakra-ui/layout";
 import {Authed, CashierOnly, ShopOnly} from "@/lib/e-syoku-api/AuthTypeProvider";
 import {Box, Flex, Spacer, Text} from "@chakra-ui/react";
 import Link from "next/link";
+import {challengeAndLoginPush, useFirebaseAuth} from "@/lib/firebase/authentication";
+import {useEffect} from "react";
+import {useRouter} from "next/navigation";
 
 export default function Home() {
+    const auth = useFirebaseAuth()
+    const router = useRouter()
+    useEffect(() => {
+        if (auth.auth) {
+            challengeAndLoginPush(auth.auth, router, "/")
+        }
+    }, [auth.auth]);
+
     return (
         <Center h={"calc(100dvh - 1rem - 24px - 8px)"} w={"full"} p={2}>
             <Flex h={"full"} w={"67%"} direction={"column"} alignItems={"center"}>
-                <Authed types={["ANONYMOUS","ADMIN"]} success={(type) => {
+                <Authed types={["ANONYMOUS", "ADMIN"]} success={(type) => {
                     return (
                         <>
                             <LinkBtn href="/buy" text="新規購入"/>
