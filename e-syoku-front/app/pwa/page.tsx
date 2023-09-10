2304815d9e0e999ef2f7f4e5c9a9520c39663345
bun133 <bun133.work@gmail.com>
2023-09-10 13:25:26 +0900

    fix building problem

--- e-syoku-front/app/pwa/page.tsx
+++ e-syoku-front/app/pwa/page.tsx
@@ -2,16 +2,26 @@
 import {MdComponent} from "@/components/error/ErrorMdComponent";
 import {VStack} from "@chakra-ui/layout";
 import Btn from "@/components/btn";
+import {useEffect, useState} from "react";
+import {useSearchParams} from "next/navigation";
 
 export default function Page() {
+    // force client-side rendering
+    const _ = useSearchParams()
+
     function isPWA() {
         return window.matchMedia('(display-mode: standalone)').matches
     }
 
+    const [homeBtn, setHomeBtn] = useState(false)
+    useEffect(() => {
+        setHomeBtn(isPWA())
+    }, [])
+
     return (
         <VStack>
             <MdComponent mdFileName={"pwa.md"}/>
-            {isPWA() && <Btn href={"/"}>ホームへ</Btn>}
+            {homeBtn && <Btn href={"/"}>ホームへ</Btn>}
         </VStack>
     )
 }
--- e-syoku-front/app/terms/page.tsx
+++ e-syoku-front/app/terms/page.tsx
@@ -1,7 +1,10 @@
 "use client"
 import {MdComponent} from "@/components/error/ErrorMdComponent";
+import {useSearchParams} from "next/navigation";
 
 export default function Page(){
+    // force client-side rendering
+    const _ = useSearchParams()
     return (
         <MdComponent mdFileName={"terms.md"}/>
     )
