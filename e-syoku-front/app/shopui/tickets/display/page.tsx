a2d22c8019edd0edefc279554df28a43b23e6fb9
bun133 <bun133.work@gmail.com>
2023-09-13 08:57:55 +0900

    TicketDisplay すべての店舗を一画面で見れるように

--- e-syoku-front/app/shopui/tickets/display/page.tsx
+++ e-syoku-front/app/shopui/tickets/display/page.tsx
@@ -1,32 +1,25 @@
 "use client"
 import {APIEndpoint} from "@/lib/e-syoku-api/APIEndpointComponent";
-import {listShopsEndPoint} from "@/lib/e-syoku-api/EndPoints";
-import {Container} from "@chakra-ui/react";
-import {Center, VStack} from "@chakra-ui/layout";
-import Btn from "@/components/btn";
+import {listShopsEndPoint, ticketDisplayEndpoint} from "@/lib/e-syoku-api/EndPoints";
 import {useRouter} from "next/navigation";
-import PageTitle from "@/components/pageTitle";
+import {VStack} from "@chakra-ui/react";
+import {TicketDisplay} from "@/components/TicketDisplay";
 
 export default function Page() {
     const router = useRouter()
     return (
         <>
-            <PageTitle title={"店舗選択"}/>
             <APIEndpoint endpoint={listShopsEndPoint} query={{}} onEnd={(data) => {
-                return (
-                    <Container>
-                        <VStack>
-                            <Center>店舗を選んでください</Center>
-                            {data.data.shops.map((shop) => {
-                                return (
-                                    <Btn onClick={() => {
-                                        router.push(`/shopui/tickets/display/id?shopId=${shop.shopId}`)
-                                    }} key={shop.shopId}>{shop.name}</Btn>
-                                )
-                            })}
-                        </VStack>
-                    </Container>
-                )
+                return (<VStack>
+                    {data.data.shops.map((e) => {
+                        return (
+                            <APIEndpoint key={e.shopId} endpoint={ticketDisplayEndpoint} query={{shopId: e.shopId}}
+                                         onEnd={(display) => {
+                                             return <TicketDisplay tickets={display.data.tickets}/>
+                                         }}/>
+                        )
+                    })}
+                </VStack>)
             }}/>
         </>
     )
